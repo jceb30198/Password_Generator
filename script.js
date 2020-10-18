6353565e472b710c5412d9f50d0256e334c3c5f0
Jack Ceballos <jackrceballos@gmail.com>
2020-10-17 20:19:26 -0400

    Final password generator script.

--- script.js
+++ script.js
@@ -13,56 +13,72 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+  
+function generatePassword() {
 
-  function generatePassword() {
+  // Prompt to determine password length.
+  var passwordLength = parseInt(prompt("How long would you like the password to be? Please choose between 8 and 128 characters."));
     
-    // Prompt to determine password length.
-    var passwordLength = parseInt(prompt("How long would you like the password to be? Please choose between 8 and 128 characters."));
-    
-    // If else statement for determining length of password.
-    if (passwordLength === "" || passwordLength < 8 || passwordLength > 128) {
-      alert("Please input a number between 8 and 128.");
-      console.log("error"); // Test Point
-    }
-    else {
-      // Booleans to confirm whether to put variables into generator. Only if password is between 8 and 128.
-      var confirmNumber = confirm("Would you like numbers in the password?");
-      var confirmSpecial = confirm("Would you like special characters in the password?");
-      var confirmLowercase = confirm("Would you like lowercase letters in the password?");
-      var confirmUppercase = confirm("Would you like uppercase letters in the password?");
-    }
-
-    // Booleans confirm check.
-    if (confirmNumber == false && confirmSpecial == false && confirmUppercase == false && confirmLowercase == false) {
-      alert("Must accept at least one of the criteria.");
-
-      console.log("error"); // Test Point
-    }
-
-    if (confirmNumber) {
-      
-
-      console.log("success_1"); // Test Point
-    }
-
-    if (confirmSpecial) {
-
-
-      console.log("success_2"); // Test Point
-    }
-
-   if (confirmLowercase) {
-
-
-      console.log("success_3"); // Test Point
-    }
-
-    if (confirmUppercase) {
-
-
-      console.log("success_4"); // Test Point
-    }
+  // If else statement for determining length of password.
+  if (passwordLength === "" || passwordLength < 8 || passwordLength > 128) {
+    alert("Please input a number between 8 and 128.");
+    console.log("error"); // Test Point
   }
+  else {
+    // Booleans to confirm whether to put variables into generator. Only if password is between 8 and 128.
+    var confirmNumber = confirm("Would you like numbers in the password?");
+    var confirmSpecial = confirm("Would you like special characters in the password?");
+    var confirmLowercase = confirm("Would you like lowercase letters in the password?");
+    var confirmUppercase = confirm("Would you like uppercase letters in the password?");
+  }
+
+  // Booleans confirm check.
+  if (confirmNumber == false && confirmSpecial == false && confirmUppercase == false && confirmLowercase == false) {
+    alert("Must accept at least one of the criteria.");
+
+    console.log("error"); // Test Point
+  }
+
+  // Collection of the different characters.
+  var passwordArray = [];
+  
+  // Character Selection.
+  if (confirmNumber) {
+    var passwordArray = passwordArray.concat(numberCharacter);
+
+    console.log("success_1"); // Test Point
+  }
+
+  if (confirmSpecial) {
+    var passwordArray = passwordArray.concat(specialCharacter);
+
+    console.log("success_2"); // Test Point
+  }
+
+  if (confirmLowercase) {
+    var passwordArray = passwordArray.concat(lowercaseCharacter);
+
+    console.log("success_3"); // Test Point
+  }
+
+  if (confirmUppercase) {
+    var passwordArray = passwordArray.concat(uppercaseCharacter);
+
+    console.log("success_4"); // Test Point
+  }
+  
+  // String must be outside of looop because it will show up as undefined if it is inside.
+  var finalPassword = ""
+
+  // Loop to make sure that the length of the password is met.
+  for ( var i = 0; i < passwordLength; i++) {
+    finalPassword = finalPassword + passwordArray[Math.floor(Math.random() * passwordArray.length)];
+
+    console.log(finalPassword); // Test Point
+  }
+
+  return finalPassword;
 }
 
 // Add event listener to generate button
